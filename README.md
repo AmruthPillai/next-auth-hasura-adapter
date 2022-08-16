09f6c08e53da0bbd8fd77669ae1e9e9a3ce8694b
Amruth Pillai <im.amruth@gmail.com>
2022-08-16 14:45:14 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,127 @@
+<p align="center">
+   <br/>
+   <a href="https://next-auth.js.org" target="_blank"><img height="64px" src="https://next-auth.js.org/img/logo/logo-sm.png" /></a>&nbsp;&nbsp;&nbsp;&nbsp;<img height="64px" src="https://user-images.githubusercontent.com/1134738/184874569-9ebc18c4-aa3f-40cd-baa8-3663ff1f8c80.png" />
+   <h3 align="center"><b>Hasura Adapter</b> - NextAuth.js</h3>
+   <p align="center">
+   Open Source. Full Stack. Own Your Data.
+   </p>
+   <p align="center" style="align: center;">
+      <img src="https://github.com/nextauthjs/next-auth/actions/workflows/release.yml/badge.svg?branch=main" alt="CI Test" />
+      <a href="https://www.npmjs.com/package/next-auth-hasura-adapter" target="_blank"><img src="https://img.shields.io/bundlephobia/minzip/next-auth-hasura-adapter/next" alt="Bundle Size"/></a>
+      <a href="https://www.npmjs.com/package/next-auth-hasura-adapter" target="_blank"><img src="https://img.shields.io/npm/v/next-auth-hasura-adapter/next" alt="next-auth-hasura-adapter Version" /></a>
+   </p>
+</p>
+
+## Overview
+
+This is the Prisma Adapter for [`next-auth`](https://next-auth.js.org). This package can only be used in conjunction with the primary `next-auth` package. It is not a standalone package.
+
+You can find the Prisma schema in the docs at [next-auth.js.org/adapters/prisma](https://next-auth.js.org/adapters/prisma).
+
+## Getting Started
+
+1. Install `next-auth` and `next-auth-hasura-adapter` as well as `hasura-cli`.
+
+```sh
+npm install next-auth next-auth-hasura-adapter hasura-cli
+```
+
+2. Install the Hasura CLI, and dive into the console
+
+```sh
+hasura init
+cd hasura && hasura console
+```
+
+3. Add the NextAuth models to your database through SQL
+
+```sql
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+SET check_function_bodies = false;
+
+CREATE TABLE public.accounts (
+    id uuid DEFAULT public.gen_random_uuid() NOT NULL,
+    type text NOT NULL,
+    provider text NOT NULL,
+    "providerAccountId" text NOT NULL,
+    refresh_token text,
+    access_token text,
+    expires_at bigint,
+    token_type text,
+    scope text,
+    id_token text,
+    session_state text,
+    oauth_token_secret text,
+    oauth_token text,
+    "userId" uuid NOT NULL,
+    refresh_token_expires_in bigint
+);
+
+CREATE TABLE public.sessions (
+    id uuid DEFAULT public.gen_random_uuid() NOT NULL,
+    "sessionToken" text NOT NULL,
+    "userId" uuid NOT NULL,
+    expires timestamptz
+);
+
+CREATE TABLE public.users (
+    id uuid DEFAULT public.gen_random_uuid() NOT NULL,
+    name text,
+    email text,
+    "emailVerified" timestamptz,
+    image text
+);
+
+CREATE TABLE public.verification_tokens (
+    token text NOT NULL,
+    identifier text NOT NULL,
+    expires timestamptz
+);
+
+ALTER TABLE ONLY public.accounts
+    ADD CONSTRAINT accounts_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY public.sessions
+    ADD CONSTRAINT sessions_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_email_key UNIQUE (email);
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY public.verification_tokens
+    ADD CONSTRAINT verification_tokens_pkey PRIMARY KEY (token);
+
+ALTER TABLE ONLY public.accounts
+    ADD CONSTRAINT "accounts_userId_fkey" FOREIGN KEY ("userId") REFERENCES public.users(id) ON UPDATE RESTRICT ON DELETE CASCADE;
+
+ALTER TABLE ONLY public.sessions
+    ADD CONSTRAINT "sessions_userId_fkey" FOREIGN KEY ("userId") REFERENCES public.users(id) ON UPDATE RESTRICT ON DELETE CASCADE;
+```
+
+You can also find this file in `src/data/nextauth.sql`.
+
+4. Add this adapter to your `pages/api/[...nextauth].js` next-auth configuration object.
+
+```js
+import NextAuth from "next-auth";
+import { HasuraAdapter } from "next-auth-hasura-adapter";
+
+export default NextAuth({
+  providers: [],
+  adapter: HasuraAdapter({
+    endpoint: HASURA_API_ENPOINT,
+    admin_secret: HASURA_ADMIN_SECRET,
+  }),
+});
+```
+
+## Contributing
+
+We're open to all community contributions! If you'd like to contribute in any way, please read our [Contributing Guide](https://github.com/nextauthjs/next-auth/blob/main/CONTRIBUTING.md).
+
+## License
+
+ISC
---
+++ src/index.ts
@@ -0,0 +1,171 @@
+import { GraphQLClient } from "graphql-request";
+import { Account } from "next-auth";
+import {
+  Adapter,
+  AdapterSession,
+  AdapterUser,
+  VerificationToken,
+} from "next-auth/adapters";
+import { getSdk } from "./generated/graphql";
+
+type HasuraAdapterArgs = {
+  endpoint: string;
+  adminSecret: string;
+};
+
+const dateTransformer = <T extends { [key: string]: unknown }>(
+  object: T | null | undefined,
+  keys?: (keyof T)[]
+): T | null | undefined => {
+  if (!object) return;
+
+  Object.entries(object).reduce(
+    (acc: Record<string, unknown>, [key, value]) => {
+      if (keys && keys.includes(key)) {
+        acc[key] = new Date(value as string);
+      } else {
+        acc[key] = value;
+      }
+
+      return acc;
+    },
+    {}
+  );
+};
+
+export const HasuraAdapter = ({
+  endpoint,
+  adminSecret,
+}: HasuraAdapterArgs): Adapter => {
+  const client = new GraphQLClient(endpoint, {
+    headers: {
+      ["x-hasura-admin-secret"]: adminSecret,
+    },
+  });
+
+  const sdk = getSdk(client);
+
+  return {
+    // User
+    createUser: async (data) => {
+      const res = await sdk.CreateUser({ data });
+      const user = dateTransformer(res?.insert_users_one, ["emailVerified"]);
+
+      return user as AdapterUser;
+    },
+    getUser: async (id) => {
+      const res = await sdk.GetUser({ id });
+      const user = dateTransformer(res?.users_by_pk, ["emailVerified"]);
+
+      return user as AdapterUser;
+    },
+    getUserByEmail: async (email) => {
+      const res = await sdk.GetUsers({ where: { email: { _eq: email } } });
+      const user = dateTransformer(res?.users?.[0], ["emailVerified"]);
+
+      if (!user) return null;
+
+      return user as AdapterUser;
+    },
+    getUserByAccount: async ({ providerAccountId, provider }) => {
+      const res = await sdk.GetUsers({
+        where: {
+          accounts: {
+            provider: { _eq: provider },
+            providerAccountId: { _eq: providerAccountId },
+          },
+        },
+      });
+      const user = dateTransformer(res?.users?.[0], ["emailVerified"]);
+
+      if (!user) return null;
+
+      return user as AdapterUser;
+    },
+    updateUser: async ({ id, ...data }) => {
+      const res = await sdk.UpdateUser({ id, data });
+      const user = dateTransformer(res?.update_users_by_pk, ["emailVerified"]);
+
+      return user as AdapterUser;
+    },
+    deleteUser: async (id) => {
+      const res = await sdk.DeleteUser({ id });
+      const user = dateTransformer(res?.delete_users_by_pk, ["emailVerified"]);
+
+      return user as AdapterUser;
+    },
+    // Session
+    createSession: async (data) => {
+      const res = await sdk.CreateSession({ data });
+      const session = dateTransformer(res?.insert_sessions_one, ["expires"]);
+
+      return session as AdapterSession;
+    },
+    getSessionAndUser: async (sessionToken) => {
+      const res = await sdk.GetSession({ sessionToken });
+      const session = dateTransformer(res?.sessions?.[0], ["expires"]);
+      const user = dateTransformer(session?.user, ["emailVerified"]);
+
+      return {
+        session: session as AdapterSession,
+        user: user as AdapterUser,
+      };
+    },
+    updateSession: async ({ sessionToken, ...data }) => {
+      const res = await sdk.UpdateSession({ sessionToken, data });
+      const session = dateTransformer(res?.update_sessions?.returning?.[0], [
+        "expires",
+      ]);
+
+      if (!session) return;
+
+      return session as AdapterSession;
+    },
+    deleteSession: async (sessionToken) => {
+      const res = await sdk.DeleteSession({ sessionToken });
+      const session = dateTransformer(res?.delete_sessions?.returning?.[0], [
+        "expires",
+      ]);
+
+      if (!session) return;
+
+      return session as AdapterSession;
+    },
+    // Account
+    linkAccount: async (data) => {
+      const res = await sdk.CreateAccount({ data });
+      const account = res?.insert_accounts_one;
+
+      return account as Account;
+    },
+    unlinkAccount: async ({ providerAccountId, provider }) => {
+      const res = await sdk.DeleteAccount({ provider, providerAccountId });
+      const account = res?.delete_accounts?.returning?.[0];
+
+      if (!account) return;
+
+      return account as Account;
+    },
+    // Verification Token
+    createVerificationToken: async (data) => {
+      const res = await sdk.CreateVerificationToken({ data });
+      const verificationToken = dateTransformer(
+        res?.insert_verification_tokens_one,
+        ["expires"]
+      );
+
+      return verificationToken as VerificationToken;
+    },
+    useVerificationToken: async ({ identifier, token }) => {
+      const res = await sdk.DeleteVerificationToken({ identifier, token });
+      const verificationToken = dateTransformer(
+        res?.delete_verification_tokens?.returning?.[0],
+        ["expires"]
+      );
+
+      if (!verificationToken) return null;
+
+      return verificationToken as VerificationToken;
+    },
+  };
+};
